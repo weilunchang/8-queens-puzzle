9a9e3426ce48337369d3961880d4137b1f22fb1b
張 瑋倫 <adan611058@gmail.com>
2022-10-26 22:16:29 +0800

    加入專案檔案。

---
+++ 8-queens-puzzle/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace _8_queens_puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Cal = new System.Windows.Forms.Button();
+            this.lst_ans = new System.Windows.Forms.ListBox();
+            this.lbl_ans = new System.Windows.Forms.Label();
+            this.txt_intput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Cal
+            // 
+            this.btn_Cal.Location = new System.Drawing.Point(12, 12);
+            this.btn_Cal.Name = "btn_Cal";
+            this.btn_Cal.Size = new System.Drawing.Size(172, 56);
+            this.btn_Cal.TabIndex = 0;
+            this.btn_Cal.Text = "Calculate";
+            this.btn_Cal.UseVisualStyleBackColor = true;
+            this.btn_Cal.Click += new System.EventHandler(this.btn_Cal_Click);
+            // 
+            // lst_ans
+            // 
+            this.lst_ans.FormattingEnabled = true;
+            this.lst_ans.HorizontalScrollbar = true;
+            this.lst_ans.ItemHeight = 19;
+            this.lst_ans.Location = new System.Drawing.Point(12, 113);
+            this.lst_ans.Name = "lst_ans";
+            this.lst_ans.Size = new System.Drawing.Size(172, 232);
+            this.lst_ans.TabIndex = 1;
+            // 
+            // lbl_ans
+            // 
+            this.lbl_ans.AutoSize = true;
+            this.lbl_ans.Location = new System.Drawing.Point(12, 82);
+            this.lbl_ans.Name = "lbl_ans";
+            this.lbl_ans.Size = new System.Drawing.Size(61, 19);
+            this.lbl_ans.TabIndex = 2;
+            this.lbl_ans.Text = "Answer";
+            // 
+            // txt_intput
+            // 
+            this.txt_intput.Location = new System.Drawing.Point(190, 41);
+            this.txt_intput.Name = "txt_intput";
+            this.txt_intput.Size = new System.Drawing.Size(74, 27);
+            this.txt_intput.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 432);
+            this.Controls.Add(this.txt_intput);
+            this.Controls.Add(this.lbl_ans);
+            this.Controls.Add(this.lst_ans);
+            this.Controls.Add(this.btn_Cal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Cal;
+        private System.Windows.Forms.ListBox lst_ans;
+        private System.Windows.Forms.Label lbl_ans;
+        private System.Windows.Forms.TextBox txt_intput;
+    }
+}
+
---
+++ 8-queens-puzzle/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8_queens_puzzle
+{
+    public partial class Form1 : Form
+    {
+        //八皇后問題是一個以西洋棋為背景的問題：如何能夠在8×8的西洋棋棋盤上放置八個皇后，使得任何一個皇后都無法直接吃掉其他的皇后
+        //為了達到此目的，任兩個皇后都不能處於同一條橫行、縱行或斜線上。八皇后問題可以推廣為更一般的n皇后擺放問題：這時棋盤的大小變為n×n，
+        ///而皇后個數也變成n。若且唯若n = 1或n ≥ 4時問題有解。
+        static int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PutQueen(int n, List<int> queen, int row)
+        {
+            for (queen[row] = 1; queen[row] <= n; queen[row]++)//從第一列開始，如果已經超過列數最大值，則回到上一層
+            {
+                if (CheckQueens(queen, row))
+                {
+                    row++;
+                    if (row < n)//如果列數允許，則繼續往後放並檢查
+                    {
+                        PutQueen(n, queen, row);//
+                    }
+                    else
+                    {
+                        count++;
+                        for (int i = 0; i < n; i++)
+                        {
+                            lst_ans.Items.Add(queen[i].ToString() + " ");
+                        }
+                    }
+                    row--;//如果該行全列皆false，則退回上一行，並且上一行的queen向右位移一列
+                }
+            }
+        }
+
+        /// <summary>
+        /// Queen position check
+        /// </summary>
+        /// <param name="queen"></param>
+        /// <param name="row"></param>
+        /// <returns></returns>
+        private static bool CheckQueens(List<int> queen, int row)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                
+                int a = Math.Abs(queen[i] - queen[row]);//前一個與目前行數相減
+                int b = Math.Abs(i - row);
+                if (Math.Abs(queen[i] - queen[row]) == Math.Abs(i - row) // 檢查是否在前面queen位置的斜對角或同一列
+                    || queen[i] == queen[row])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Queen數量 queen[n]代表第n+1行 queen[n] value代表第幾列
+        /// </summary>
+        List<int> queen;
+
+        private void btn_Cal_Click(object sender, EventArgs e)
+        {
+            lst_ans.Items.Clear();
+            int n = Int32.Parse(txt_intput.Text);
+            queen = new List<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                queen.Add(0);
+            }
+            PutQueen(n, queen, 0);
+            MessageBox.Show(count.ToString());
+        }
+    }
+}
+
